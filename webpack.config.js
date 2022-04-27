cb6573c273cc3a4cb36150797054b813bca9e403
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-04-27 12:25:32 -0500

    Create index.html, Add content and set it as a template

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        template: './src/index.html',
+      }
+    ),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
