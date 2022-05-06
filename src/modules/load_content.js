5745d9e864fb1cdfb35f0a1ea045edd7a1df319e
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-05-06 14:10:32 -0500

    Refactor removeAllCompleted() function to use filter() method and meet requirements

--- src/modules/load_content.js
+++ src/modules/load_content.js
@@ -15,6 +15,7 @@ export default function loadContent(taskArr) {
     });
   } else {
     taskArr = [];
+    localStorage.setItem('taskArray', JSON.stringify(taskArr));
   }
 
   return taskArr;
--- src/modules/remove_completed.js
+++ src/modules/remove_completed.js
@@ -1,15 +1,21 @@
-import { taskList, completedElements, taskArr } from './variables.js';
+import { taskList, taskArr } from './variables.js';
 
-export default function removeAllCompleted() {
-  while (completedElements.length !== 0) {
-    taskArr.splice(completedElements[0].id, 1);
-    completedElements[0].remove();
 
-    taskArr.forEach((task, i) => {
-      task.index = i;
-      taskList.children[i].id = i;
-    });
-  }
+export default function removeAllCompleted(newArr) {
+  newArr = taskArr.filter((task, i) => {
+    if (task.completed) {
+      document.getElementById(i).remove();
+    }
+    return task.completed !== true;
+  });
+
+  taskArr.length = 0;
+  taskArr.push(...newArr);
+
+  taskArr.forEach((task, i) => {
+    task.index = i;
+    taskList.children[i].id = i;
+  });
 
   localStorage.setItem('taskArray', JSON.stringify(taskArr));
 }
