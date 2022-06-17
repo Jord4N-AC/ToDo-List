de92398a481b5b0b2cb81ddaced9f162a0a02c83
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-06-16 22:02:05 -0500

    Mock HTML to test DOM manipulation functions

--- src/tests/edit_update_clearCompleated.test.js
+++ src/tests/edit_update_clearCompleated.test.js
@@ -7,3 +7,23 @@ import updateCounters from '../modules/counters.js';
 import createAppendTask from '../modules/create_append.js';
 import removeTask from '../modules/remove_task.js';
 
+
+
+
+document.body.innerHTML = `
+  <div>
+    <div id="count-conter" class="counter-column">
+      <span id="all-counter" class=""></span>
+      <span id="pending-counter" class=""></span>
+      <span id="completed-counter" class=""></span>
+    </div>
+    <!-- Task Form for Adding Tasks -->
+    <form id="list-input-group" class="box-format">
+        <input id="list-input-text" type="text" placeholder="Add to your list....." required autofocus>
+        <input id="list-input-btn" class="pointer" type="submit" value="&#8626;" title="Add button">
+    </form>
+    <!-- Tasks List -->
+    <ul id="list-tasks"></ul>
+    <!-- Clear All Completed Button -->
+    <button id="clear-btn" class="box-format pointer" type="button">Clear all completed</button>
+  </div>`;
