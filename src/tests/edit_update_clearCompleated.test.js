98b9a9268066f180b29c45775532d26b2a9e422f
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-06-16 21:59:45 -0500

    Import all necessary functions

--- src/tests/edit_update_clearCompleated.test.js
+++ src/tests/edit_update_clearCompleated.test.js
@@ -1,47 +1,9 @@
-import removeAllCompleted from "../modules/remove_completed";
-import { updateContent } from "../modules/edit_task";
+import removeAllCompleted from '../modules/remove_completed.js';
 
-describe('Edit, Update status, and clear complete functions testing', () =>{
-  test('removeAllCompleted function', () => {
-    document.body.innerHTML = `
-    <button id="clear-btn" class="box-format pointer" type="button">Clear all completed</button>`;
+import { checkStatus, checkTask } from '../modules/task_status.js';
+import { saveOldContent, updateContent } from '../modules/edit_task.js';
+import updateCounters from '../modules/counters.js';
 
-    // const taskArr = [{description: 'Task 1', completed: false, index: 0},
-    // {description: 'Task2', completed: true, index: 1},
-    // {description: 'Task3', completed: false, index: 2}];
+import createAppendTask from '../modules/create_append.js';
+import removeTask from '../modules/remove_task.js';
 
-    // const button = document.querySelector('#clear-btn');
-
-    // button.addEventListener('click', removeAllCompleted);
-
-    // button.click();
-
-    removeAllCompleted();
-
-    expect(taskArr).toHaveLength(2);
-  });
-
-  // test('updateContent function', () => {
-  //   document.body.innerHTML = `
-  //     <div>
-  //       <ul id="list-tasks">
-  //         <li id="0" class="task-item box-format">
-  //             <div class="task-content">
-  //                 <input class="pointer task-input" type="checkbox">
-  //                 <label class="task-label" contenteditable="true">Task 1</label>
-  //             </div>
-  //             <div class="task-icons">
-  //                 <i class="fa fa-trash-o pointer" title="Remove task"></i>
-  //                 <i class="fa fa-ellipsis-v"></i>
-  //             </div>
-  //         </li>
-  //       </ul>
-  //     </div>`;
-
-  //     const input = document.querySelector('.task-label');
-
-  //     input.addEventListener('blur', updateContent);
-  
-  //     input.blur();
-  // });
-});
