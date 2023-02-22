f90b9d70a44f17c89140c0951a18435d16150e94
Jordan <camiloarmijosantifonamusic@gmail.com>
2023-02-22 17:41:41 -0500

    Intall and Setup html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
