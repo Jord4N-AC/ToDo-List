c99a453e715174d0aea2b4d23e1444aed4f1a94e
Jordan <camiloarmijosantifonamusic@gmail.com>
2023-02-23 11:39:16 -0500

    Create function to create and append task

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import './style.css';
 
+const taskList = document.getElementById('list-tasks');
+
+// Tasks Array
 const taskArr = [
-    { description: 'Task 1', completed: false, index: 0 },
-    { description: 'Task 2', completed: false, index: 1 },
-    { description: 'Task 3', completed: false, index: 2 },
+    { description: 'Task 1', completed: false, index: 1 },
+    { description: 'Task 2', completed: false, index: 2 },
+    { description: 'Task 3', completed: false, index: 3 },
+    { description: 'Task 4', completed: true, index: 4 },
+    { description: 'Task 5', completed: true, index: 5 },
 ];
+
+// Create and Append Task
+const createAppendTask = ({ description: taskContent, completed: taskStatus }) => {
+    const taskElement = `
+              <li class="task-item box-format">
+                  <div class="task-content">
+                      <input class="pointer task-input" type="checkbox" ${taskStatus ? 'checked' : ''}>
+                      <label class="task-label">${taskContent}</label>
+                  </div>
+                  <div class="task-icons">
+                      <i class="fa fa-trash-o pointer"></i>
+                      <i class="fa fa-ellipsis-v"></i>
+                  </div>
+              </li>
+      `;
+    taskList.insertAdjacentHTML('beforeend', taskElement);
+}
